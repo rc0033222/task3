34d799d3a84d496df857d26de14c1031a1c20826
Caine Atkinson <rc0033222@student.riversidecollege.ac.uk>
2018-10-02 09:47:50 +0100

    code works for task 3

--- task3/Form1.Designer.cs
+++ task3/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.richTextBox1.Size = new System.Drawing.Size(266, 203);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // Form1
             // 
